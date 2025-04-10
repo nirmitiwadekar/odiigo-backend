9fd456e609e6f11ea57b6ca5c2c5bfbe86ac03c4
Achintya <achintyashende@gmail.com>
2025-04-10 15:47:44 +0530

    Updated user Schema

--- src/odiigo-modules/auth/models/User.js
+++ src/odiigo-modules/auth/models/User.js
@@ -2,14 +2,50 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     phone: { type: String, required: true, unique: true },
+    name: { type: String,},
+    dob: { type: Date,},
+    gender: {
+        type: String,
+        enum: ["Male", "Female", "Other", "Don't want to specify"],
+        default: "Don't want to specify"
+    },
+    email: { type: String,unique: true },
     isVerified: { type: Boolean, default: false },
     refreshToken: { type: String },
     lastLogin: { type: Date },
     location: {
         latitude: { type: Number },
-        longitude: { type: Number },
-    }, // Added location field
+        longitude: { type: Number }
+    },
+    address: [
+        {
+            street: { type: String },
+            city: { type: String, default: 'Pune' },
+            state: { type: String, default: 'Maharashtra' },
+            zipCode: { type: String },
+            country: { type: String, default: "India" }
+        }
+    ],
+    profilePic: { type: String, default: "default-profile.jpg" },
+    vehicle: [{ type: mongoose.Schema.Types.ObjectId, ref: "Vehicle" }],
+    isActive: { type: Boolean, default: true }
 }, { timestamps: true });
 
-const Users = mongoose.model('User', userSchema);
-module.exports = Users;
+module.exports = mongoose.model('User', userSchema);
+
+
+// const mongoose = require('mongoose');
+
+// const userSchema = new mongoose.Schema({
+//     phone: { type: String, required: true, unique: true },
+//     isVerified: { type: Boolean, default: false },
+//     refreshToken: { type: String },
+//     lastLogin: { type: Date },
+//     location: {
+//         latitude: { type: Number },
+//         longitude: { type: Number },
+//     }, // Added location field
+// }, { timestamps: true });
+
+// const Users = mongoose.model('User', userSchema);
+// module.exports = Users;
